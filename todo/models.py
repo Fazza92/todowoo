a58b167669a3fd73a7ce5e17e3d5f0bcd9776ddc
Oscar <oscarwasil@gmail.com>
2021-05-30 12:39:24 -0500

    Added delete and complete function

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models import Model
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+  name = models.CharField(max_length=100)
+  memo = models.TextField(blank=True)
+  datecompleted = models.DateTimeField(null=True)
+  created = models.DateTimeField(auto_now_add=True)
+  important = models.BooleanField(default=False)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.name
+  
